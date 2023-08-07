b3a6f19950f009fb8dc6e89ca9563b9c42c63426
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-07 17:39:59 +0200

    Update moderator.py

--- app/models/moderator.py
+++ app/models/moderator.py
@@ -1,1 +1,10 @@
+from app import db
+
+class Moderator(db.Model):
+    __tablename__ = "moderators"
+
+    id = db.Column(db.Integer, primary_key=True)
+    moderator_id = db.Column(db.String(80), unique=True, nullable=False)
+    associated_university = db.Column(db.String(250), nullable=False)
+    permissions = db.Column(db.String(500))  # This can be improved with a proper permissioning system later
 
