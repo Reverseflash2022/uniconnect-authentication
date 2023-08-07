6c9d190cb7de6bc9afe44bebd02f8467e1976011
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-07 17:56:12 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_limiter import Limiter
+from app.utils.error_handlers import error_handlers_blueprint
 from os import getenv
 from dotenv import load_dotenv
 
@@ -14,4 +16,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{getenv('MYSQL_USER')}:{getenv
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
+limiter = Limiter(app, key_func=get_remote_address) # Rate limiting
+
+# Register blueprints
+app.register_blueprint(error_handlers_blueprint)
+
+
+
+
+
 
