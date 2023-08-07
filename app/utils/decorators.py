f0717e508a1b4280a6598c755dee38b815d67d4a
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-07 17:50:44 +0200

    Update decorators.py

--- app/utils/decorators.py
+++ app/utils/decorators.py
@@ -1,1 +1,24 @@
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
+from functools import wraps
+from flask import jsonify
+
+def student_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        verify_jwt_in_request()
+        identity = get_jwt_identity()
+        if not identity or identity.get('role') != 'student':
+            return jsonify({'message': 'Permission required: Student'}), 403
+        return fn(*args, **kwargs)
+    return wrapper
+
+def moderator_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        verify_jwt_in_request()
+        identity = get_jwt_identity()
+        if not identity or identity.get('role') != 'moderator':
+            return jsonify({'message': 'Permission required: Moderator'}), 403
+        return fn(*args, **kwargs)
+    return wrapper
 
