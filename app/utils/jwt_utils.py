873297bb6e96782942640ffc8b85b5e26e1c3f18
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-07 17:42:43 +0200

    Update jwt_utils.py

--- app/utils/jwt_utils.py
+++ app/utils/jwt_utils.py
@@ -1,1 +1,32 @@
+from flask_jwt_extended import create_access_token, decode_token, get_jwt_identity
+
+def encode_auth_token(user_id, role):
+    """
+    Generates the Auth Token.
+    :return: string
+    """
+    payload = {
+        'id': user_id,
+        'role': role
+    }
+    return create_access_token(identity=payload)
+
+def decode_auth_token(auth_token):
+    """
+    Decodes the auth token.
+    :return: integer|string
+    """
+    try:
+        payload = decode_token(auth_token)
+        return payload['identity']
+    except:
+        return None
+
+def get_current_user_role():
+    """
+    Returns the role of the current user.
+    :return: string
+    """
+    identity = get_jwt_identity()
+    return identity['role'] if identity else None
 
