718298722570780d4412832bfe0b7bb1b3315cd5
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-07 17:48:55 +0200

    Update jwt_utils.py

--- app/utils/jwt_utils.py
+++ app/utils/jwt_utils.py
@@ -1,26 +1,34 @@
-from flask_jwt_extended import create_access_token, decode_token, get_jwt_identity
+from flask_jwt_extended import get_jwt_identity
+import jwt
+from datetime import datetime, timedelta
+from flask import current_app
 
-def encode_auth_token(user_id, role):
-    """
-    Generates the Auth Token.
-    :return: string
-    """
-    payload = {
-        'id': user_id,
-        'role': role
-    }
-    return create_access_token(identity=payload)
+def encode_auth_token(user_id, user_role):
+    """ Generates the Auth Token """
+    try:
+        payload = {
+            'exp': datetime.utcnow() + timedelta(days=1),
+            'iat': datetime.utcnow(),
+            'sub': user_id,
+            'role': user_role
+        }
+        return jwt.encode(
+            payload,
+            current_app.config.get('SECRET_KEY'),
+            algorithm='HS256'
+        )
+    except Exception as e:
+        return str(e)
 
 def decode_auth_token(auth_token):
-    """
-    Decodes the auth token.
-    :return: integer|string
-    """
+    """ Decodes the auth token """
     try:
-        payload = decode_token(auth_token)
-        return payload['identity']
-    except:
-        return None
+        payload = jwt.decode(auth_token, current_app.config.get('SECRET_KEY'), algorithms=["HS256"])
+        return payload['sub'], payload['role']
+    except jwt.ExpiredSignatureError:
+        return 'Token expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.'
 
 def get_current_user_role():
     """
